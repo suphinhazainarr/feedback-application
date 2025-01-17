8e8ef335d66e3da8c32f5d4bcbd58cc2ac0110e9
suphinhazainarr <suphinhassainar2004@gmail.com>
2025-01-17 19:47:53 +0530

    Initial commit

---
+++ frontend/.angular/cache/19.0.7/feedback-application/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,38 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  setAngularAppEngineManifest,
+  setAngularAppManifest
+} from "./chunk-3K26IEJ5.js";
+import "./chunk-3SQOMOHA.js";
+import "./chunk-SJKYOEKN.js";
+import "./chunk-U5TFM7JB.js";
+import "./chunk-DSDJIFSG.js";
+import "./chunk-IWBMBGJL.js";
+import "./chunk-EE77CIVT.js";
+import "./chunk-VKJO5GQP.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
+//# sourceMappingURL=@angular_ssr.js.map
---
+++ frontend/.angular/cache/19.0.7/feedback-application/vite/deps_ssr/rxjs_operators.js
@@ -0,0 +1,7 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  require_operators
+} from "./chunk-VKJO5GQP.js";
+import "./chunk-YHCV7DAQ.js";
+export default require_operators();
+//# sourceMappingURL=rxjs_operators.js.map
